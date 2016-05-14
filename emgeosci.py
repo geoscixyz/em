30130d93327e3edefc0fecf58d1320ae27150fcb
Lindsey Heagy <lindseyheagy@gmail.com>
2016-05-14 16:19:24 -0700

    remove simpeg specific content from emgeosci app

--- emgeosci.py
+++ emgeosci.py
@@ -49,16 +49,6 @@ def setTemplate(self, template_values, templateFile):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        # packages = [
-        #                 dict(name="SimPEG", link="simpeg", status="check", color="green", description="A framework for simulation and gradient based parameter estimation in geophysics."),
-        #                 dict(name="simpegEM", link="simpegem", status="refresh", color="green", description="A electromagnetic forward modeling and inversion package for SimPEG."),
-        #                 dict(name="simpegMT", link="simpegmt", status="refresh", color="orange", description="Magnetotellurics forward and inverse codes for SimPEG"),
-        #                 dict(name="simpegFLOW", link="simpegflow", status="flask", color="green", description="Groundwater (vadose zone) flow equations written in the SimPEG framework."),
-        #                 dict(name="simpegDC", link="simpegdc", status="flask", color="orange", description="A DC resistivity forward modelling and inversion package for SimPEG."),
-        #                 dict(name="simpegPF", link="simpegpf", status="flask", color="orange", description="Potential fields codes for SimPEG. Gravity and Magnetics."),
-        #                 dict(name="simpegSEIS", link="simpegseis", status="wrench", color="grey", description="Time and frequency domain forward modeling and inversion of seismic wave."),
-        #                 dict(name="simpegGPR", link="simpeggpr", status="wrench", color="grey", description="Forward modelling and inversion of Ground-Penetrating Radar (GPR)."),
-        #            ]
         setTemplate(self, {"indexPage":True}, 'index.html')
 
 app = webapp2.WSGIApplication([
