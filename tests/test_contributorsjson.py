ab0f6b20d443d0897f0ce13a8720ad1ad8729624
Lindsey Heagy <lindseyheagy@gmail.com>
2016-06-29 12:52:40 -0700

    add contributors.json, add test for contributors.json validity

---
+++ tests/test_contributorsjson.py
@@ -0,0 +1,29 @@
+import subprocess
+import unittest
+import os
+import json
+
+PATH2JSON = '..'
+
+class ContribsJson_Test(unittest.TestCase):
+
+    def test_contributorsjson(self):
+
+        print '\nTesting contributors.json validity'
+
+        contribsjson = os.path.sep.join([PATH2JSON] + ['contributors.json'])
+        contribs = open(contribsjson)
+
+        try:
+            json.load(contribs)
+
+        except Exception as err:
+            print("<<<<< FAILED: contributors.json invalid >>>>> \n Error: {0}".format(err))
+            raise
+
+        print '... ok'
+        return True
+
+
+if __name__ == '__main__':
+    unittest.main()
