66e8316daea60ea0994a2dd5c9b2cfa6521b587e
Lindsey Heagy <lindseyheagy@gmail.com>
2019-01-01 11:15:17 -0700

    update requirements

--- _ext/__init__.py
+++ _ext/__init__.py
@@ -1,7 +1,7 @@
 from autodoc import (
     make_formula_sheet, make_contributorslist, make_case_histories
 )
-from environmentSetup import supress_nonlocal_image_warn, checkDependencies
+from environmentSetup import supress_nonlocal_image_warn, checkDependencies, supress_citation_not_referenced
 from copyImages import copyImages
 from edit_on_github import *
 from purpose import *
