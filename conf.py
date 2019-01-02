54e05010d0d7aed40298ecd06e319c366b5c6f61
Lindsey Heagy <lindseyheagy@gmail.com>
2019-01-02 11:59:33 -0700

    ignore lib in testing

--- conf.py
+++ conf.py
@@ -120,7 +120,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = [
-    '_build', 'AUTHORS.rst', 'README.md', '.ipynb_checkpoints', 'lib/*.rst'
+    '_build', 'AUTHORS.rst', 'README.md', '.ipynb_checkpoints', 'lib/*.rst',
     'content/equation_bank/*',
     'content/maxwell1_fundamentals/maxwell_variables.rst',
     'error.rst',
